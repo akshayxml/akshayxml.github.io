b07a2b2838ce30c0f4ef7d1959265d0412f21586
m.akshay <akshayknr7@gmail.com>
2024-05-26 12:43:35 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-30277679e33a2b276830.js"
+    "url": "webpack-runtime-241351153e743d21716a.js"
   },
   {
     "url": "framework-d878d636fe323f0e401f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1685973358f29c8cd58a3e2c81119fde"
+    "revision": "25251402af3fbf455e0e106c305ab3c1"
   },
   {
     "url": "manifest.webmanifest",
