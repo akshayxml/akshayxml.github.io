90a13556d677fa5b5ea088023c258202e7c6d995
m.akshay <akshayknr7@gmail.com>
2024-05-30 11:13:45 +0530

    refactored

---
+++ gatsby-browser.js
@@ -0,0 +1,45 @@
+function initGTMOnEvent(event) {
+    initGTM();
+    event.currentTarget.removeEventListener(event.type, initGTMOnEvent);
+}
+
+function initGTM() {
+    if (window.gtmDidInit) {
+        return false;
+    }
+
+    window.gtmDidInit = true;
+
+    let script = document.createElement('script');
+
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtm.js?id=${process.env.GATSBY_GTAG_MANAGER_ID}`;
+
+    script.onload = function() {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            // ensure PageViews are always tracked
+            event: 'gtm.js',
+            'gtm.start': new Date().getTime(),
+            'gtm.uniqueEventId': 0,
+        });
+    };
+
+    document.head.appendChild(script);
+
+    let noscript = document.createElement('noscript');
+    noscript.innerHTML = `<iframe src=\"https://www.googletagmanager.com/ns.html?id=GTM-TPLCW4NT\" height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe>`
+
+    document.body.appendChild(noscript);
+}
+
+exports.onClientEntry = function() {
+    document.onreadystatechange = function() {
+        if (document.readyState !== 'loading') setTimeout(initGTM, 1000);
+    };
+
+    document.addEventListener('scroll', initGTMOnEvent);
+    document.addEventListener('mousemove', initGTMOnEvent);
+    document.addEventListener('touchstart', initGTMOnEvent);
+};
